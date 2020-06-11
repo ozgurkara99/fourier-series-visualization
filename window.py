d306156de94da8497a8689cb9a1e64a1ef5f5383
Özgür Kara <49339814+ozgurkara99@users.noreply.github.com>
2020-06-11 19:05:01 +0300

    Add files via upload

--- window.py
+++ window.py
@@ -3,103 +3,127 @@ from mplwidget import MplWidget
 
 class Window(object):
     def setupUi(self, MainWindow):
+        
         MainWindow.setObjectName("MainWindow")
-        MainWindow.resize(640, 640)
+        MainWindow.resize(640, 840)
         self.centralwidget = QtWidgets.QWidget(MainWindow)
         self.centralwidget.setObjectName("centralwidget")
+        
         self.button_plot = QtWidgets.QPushButton(self.centralwidget)
-        self.button_plot.setGeometry(QtCore.QRect(220, 520, 100, 30))
+        self.button_plot.setGeometry(QtCore.QRect(220, 720, 100, 30))
         self.button_plot.setObjectName("button_plot")
         
         self.button_exit = QtWidgets.QPushButton(self.centralwidget)
-        self.button_exit.setGeometry(QtCore.QRect(340, 520, 100, 30))
+        self.button_exit.setGeometry(QtCore.QRect(340, 720, 100, 30))
         self.button_exit.setObjectName("button_exit")
         
         self.MplWidget = MplWidget(self.centralwidget)
-        self.MplWidget.setGeometry(QtCore.QRect(40, 30, 560, 240))
+        self.MplWidget.setGeometry(QtCore.QRect(40, 30, 560, 220))
         self.MplWidget.setObjectName("MplWidget")
         
+        self.MplWidget2 = MplWidget(self.centralwidget)
+        self.MplWidget2.setGeometry(QtCore.QRect(40, 250, 270, 220))
+        self.MplWidget2.setObjectName("MplWidget2")
+        
+        self.MplWidget3 = MplWidget(self.centralwidget)
+        self.MplWidget3.setGeometry(QtCore.QRect(330, 250, 270, 220))
+        self.MplWidget3.setObjectName("MplWidget3")
+        
         self.label_an = QtWidgets.QLabel(self.centralwidget)
-        self.label_an.setGeometry(QtCore.QRect(125, 280, 30, 30))
+        self.label_an.setGeometry(QtCore.QRect(125, 480, 30, 30))
         font = QtGui.QFont()
         font.setPointSize(16)
         self.label_an.setFont(font)
         self.label_an.setObjectName("label_an")
+        
         self.label_bn = QtWidgets.QLabel(self.centralwidget)
-        self.label_bn.setGeometry(QtCore.QRect(485, 280, 30, 30))
+        self.label_bn.setGeometry(QtCore.QRect(485, 480, 30, 30))
         font = QtGui.QFont()
         font.setPointSize(16)
         self.label_bn.setFont(font)
         self.label_bn.setObjectName("label_bn")
+        
         self.text_an = QtWidgets.QTextEdit(self.centralwidget)
-        self.text_an.setGeometry(QtCore.QRect(40, 320, 200, 165))
+        self.text_an.setGeometry(QtCore.QRect(40, 520, 200, 165))
         self.text_an.setObjectName("text_an")
+        
         self.text_bn = QtWidgets.QTextEdit(self.centralwidget)
-        self.text_bn.setGeometry(QtCore.QRect(400, 320, 200, 165))
+        self.text_bn.setGeometry(QtCore.QRect(400, 520, 200, 165))
         self.text_bn.setObjectName("text_bn")
         
         self.label_a0 = QtWidgets.QLabel(self.centralwidget)
-        self.label_a0.setGeometry(QtCore.QRect(250, 285, 60, 30))
+        self.label_a0.setGeometry(QtCore.QRect(250, 485, 60, 30))
         font = QtGui.QFont()
         font.setPointSize(12)
         self.label_a0.setFont(font)
         self.label_a0.setObjectName("label_a0") 
         
         self.label_mod = QtWidgets.QLabel(self.centralwidget)
-        self.label_mod.setGeometry(QtCore.QRect(250, 320, 60, 30))
+        self.label_mod.setGeometry(QtCore.QRect(250, 520, 60, 30))
         font = QtGui.QFont()
         font.setPointSize(12)
         self.label_mod.setFont(font)
         self.label_mod.setObjectName("label_mod")
+        
         self.label_xstart = QtWidgets.QLabel(self.centralwidget)
-        self.label_xstart.setGeometry(QtCore.QRect(250, 355, 60, 30))
+        self.label_xstart.setGeometry(QtCore.QRect(250, 555, 60, 30))
         font = QtGui.QFont()
         font.setPointSize(12)
         self.label_xstart.setFont(font)
         self.label_xstart.setObjectName("label_xstart")
+        
         self.label_xend = QtWidgets.QLabel(self.centralwidget)
-        self.label_xend.setGeometry(QtCore.QRect(250, 390, 60, 30))
+        self.label_xend.setGeometry(QtCore.QRect(250, 590, 60, 30))
         font = QtGui.QFont()
         font.setPointSize(12)
         self.label_xend.setFont(font)
         self.label_xend.setObjectName("label_xend")
+        
         self.label_period = QtWidgets.QLabel(self.centralwidget)
-        self.label_period.setGeometry(QtCore.QRect(250, 425, 60, 30))
+        self.label_period.setGeometry(QtCore.QRect(250, 625, 60, 30))
         font = QtGui.QFont()
         font.setPointSize(12)
         self.label_period.setFont(font)
         self.label_period.setObjectName("label_period")
+        
         self.label_n = QtWidgets.QLabel(self.centralwidget)
-        self.label_n.setGeometry(QtCore.QRect(250, 460, 60, 30))
+        self.label_n.setGeometry(QtCore.QRect(250, 660, 60, 30))
         font = QtGui.QFont()
         font.setPointSize(12)
         self.label_n.setFont(font)
         self.label_n.setObjectName("label_n")
         
         self.edit_a0 = QtWidgets.QLineEdit(self.centralwidget)
-        self.edit_a0.setGeometry(QtCore.QRect(320, 285, 70, 25))
+        self.edit_a0.setGeometry(QtCore.QRect(320, 485, 70, 25))
         self.edit_a0.setObjectName("edit_a0")
         
         self.edit_mod = QtWidgets.QLineEdit(self.centralwidget)
-        self.edit_mod.setGeometry(QtCore.QRect(320, 320, 70, 25))
+        self.edit_mod.setGeometry(QtCore.QRect(320, 520, 70, 25))
         self.edit_mod.setObjectName("edit_mod")
+        
         self.edit_xend = QtWidgets.QLineEdit(self.centralwidget)
-        self.edit_xend.setGeometry(QtCore.QRect(320, 390, 70, 25))
+        self.edit_xend.setGeometry(QtCore.QRect(320, 590, 70, 25))
         self.edit_xend.setObjectName("edit_xend")
+        
         self.edit_xstart = QtWidgets.QLineEdit(self.centralwidget)
-        self.edit_xstart.setGeometry(QtCore.QRect(320, 355, 70, 25))
+        self.edit_xstart.setGeometry(QtCore.QRect(320, 555, 70, 25))
         self.edit_xstart.setObjectName("edit_xstart")
+        
         self.edit_n = QtWidgets.QLineEdit(self.centralwidget)
-        self.edit_n.setGeometry(QtCore.QRect(320, 460, 70, 25))
+        self.edit_n.setGeometry(QtCore.QRect(320, 660, 70, 25))
         self.edit_n.setObjectName("edit_n")
+        
         self.edit_period = QtWidgets.QLineEdit(self.centralwidget)
-        self.edit_period.setGeometry(QtCore.QRect(320, 425, 70, 25))
+        self.edit_period.setGeometry(QtCore.QRect(320, 625, 70, 25))
         self.edit_period.setObjectName("edit_period")
+        
         MainWindow.setCentralWidget(self.centralwidget)
+        
         self.menubar = QtWidgets.QMenuBar(MainWindow)
         self.menubar.setGeometry(QtCore.QRect(0, 0, 640, 26))
         self.menubar.setObjectName("menubar")
         MainWindow.setMenuBar(self.menubar)
+        
         self.statusbar = QtWidgets.QStatusBar(MainWindow)
         self.statusbar.setObjectName("statusbar")
         MainWindow.setStatusBar(self.statusbar)
